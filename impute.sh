79e2a6d44d1a8f7d326024c8749922835746f02d
koichishi <goshi@ucsd.edu>
2024-03-07 23:06:41 +0000

    uncomment the beagle cmd

--- impute.sh
+++ impute.sh
@@ -16,33 +16,47 @@ awk '/^#/ {print; next} {print toupper($0)}' | \
 bgzip -c > ${data_filepath}_fixcase.vcf.gz
 tabix -p vcf ${data_filepath}_fixcase.vcf.gz
 
+# Convert into Oxford format
+# # Option 1: convert manually
 # # Convert to plink format
 # vcftools --gzvcf ${data_filepath}_fixcase.vcf.gz --plink --out ${data_filepath}_fixcase
 # # Convert to oxford format
 # plink --file ${data_filepath}_fixcase --recode oxford --out ${data_filepath}_fixcase
+# Option 2: use script
+perl -vcf ${data_filepath}_fixcase.vcf -gen ${data_filepath}_fixcase.gen
 
 # touch excludemarkers.txt
-# echo "8:89329148" >> excludemarkers.txt
-# echo "12:8400000" >> excludemarkers.txt
-# echo "14:21649957" >> excludemarkers.txt
-# echo "17:1144632" >> excludemarkers.txt
-# KGDIR=~/public/1000Genomes/
+echo "8:89329148" >> excludemarkers.txt
+echo "12:8400000" >> excludemarkers.txt
+echo "14:21649957" >> excludemarkers.txt
+echo "17:1144632" >> excludemarkers.txt
+KGDIR=~/public/1000Genomes/
 
 # # Run beagle
-# mkdir beagle
-# for chrom in $(seq 1 1)
-# do
-#     echo "Imputing chr${chrom} using Beagle"
-#     java -jar beagle.19Apr22.7c0.jar chrom=$chrom \
-#         gt=${data_filepath}_fixcase.vcf.gz \
-#         ref=${KGDIR}/ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-#         excludemarkers=excludemarkers.txt \
-#         out=./beagle/${prefix}_imputed_chr${chrom}
-# done
+mkdir beagle
+for chrom in $(seq 1 1)
+do
+    echo "Imputing chr${chrom} using Beagle"
+    java -jar beagle.19Apr22.7c0.jar chrom=$chrom \
+        gt=${data_filepath}_fixcase.vcf.gz \
+        ref=${KGDIR}/ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+        excludemarkers=excludemarkers.txt \
+        out=./beagle/${prefix}_imputed_chr${chrom}
+done
 
-# # Remove duplicates
-# zcat data/imputed_23andme_merged.vcf.gz | grep -v "^#" | cut -f 3 | sort | uniq -c | \
-#    awk '{print $1 "\t" $2}' | awk '($1>1)' | grep -v "\." | cut -f 2 > dupids_23andme.txt
+# Remove duplicates
+zcat data/imputed_23andme_merged.vcf.gz | grep -v "^#" | cut -f 3 | sort | uniq -c | \
+   awk '{print $1 "\t" $2}' | awk '($1>1)' | grep -v "\." | cut -f 2 > dupids_23andme.txt
 
-# mkdir impute
+mkdir impute
+# TODO: Prepare the gens file and mimic the following
+# ./impute2 \
+#  -m ./Example/example.chr22.map \
+#  -g_ref ./Example/example.chr22.reference.gens \
+#  -strand_g_ref ./Example/example.chr22.reference.strand \
+#  -g ./Example/example.chr22.study.gens \
+#  -strand_g ./Example/example.chr22.study.strand \
+#  -int 20.4e6 20.5e6 \
+#  -Ne 20000 \
+#  -o ./Example/example.chr22.one.unphased.impute2
 
