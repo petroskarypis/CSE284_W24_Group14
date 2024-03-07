297b4d8d27c1969d11ec3fd4cd12643b669edd29
koichishi <goshi@ucsd.edu>
2024-03-07 20:53:32 +0000

    push everything

--- impute.sh
+++ impute.sh
@@ -2,18 +2,47 @@
 
 set -e
 
-TARGETVCF=./misc/mgymrek_genome.vcf.gz
+TARGETVCF=~/public/misc/mgymrek_genome.vcf.gz
 
 prefix=$(basename $TARGETVCF .vcf.gz)
+data_filepath=./data/$prefix
 
-# Fix the VCF file to all upper case
-zcat ${TARGETVCF} | sed 's/a/A/g' | sed 's/t/T/g' | sed 's/g/G/g' | sed 's/c/C/g' | \
-    bgzip -c > ${prefix}_fixcase.vcf.gz 
-tabix -p vcf ${prefix}_fixcase.vcf.gz
+# Download beagle
+# wget https://faculty.washington.edu/browning/beagle/beagle.19Apr22.7c0.jar
 
-# (Option 2) Impute using Beagle
-eagle \
-    --vcfRef=${prefix}_fixcase.vcf.gz \
-    --vcfTarget=${prefix}_fixcase.vcf.gz \
-    --geneticMapFile=../misc/genetic_map_hg19_withX.txt.gz \
-    --outPrefix=${prefix}_imputed
+# Fix 23andme VCF
+zcat ${TARGETVCF} | \
+awk '/^#/ {print; next} {print toupper($0)}' | \
+bgzip -c > ${data_filepath}_fixcase.vcf.gz
+tabix -p vcf ${data_filepath}_fixcase.vcf.gz
+
+# # Convert to plink format
+# vcftools --gzvcf ${data_filepath}_fixcase.vcf.gz --plink --out ${data_filepath}_fixcase
+# # Convert to oxford format
+# plink --file ${data_filepath}_fixcase --recode oxford --out ${data_filepath}_fixcase
+
+# touch excludemarkers.txt
+# echo "8:89329148" >> excludemarkers.txt
+# echo "12:8400000" >> excludemarkers.txt
+# echo "14:21649957" >> excludemarkers.txt
+# echo "17:1144632" >> excludemarkers.txt
+# KGDIR=~/public/1000Genomes/
+
+# # Run beagle
+# mkdir beagle
+# for chrom in $(seq 1 1)
+# do
+#     echo "Imputing chr${chrom} using Beagle"
+#     java -jar beagle.19Apr22.7c0.jar chrom=$chrom \
+#         gt=${data_filepath}_fixcase.vcf.gz \
+#         ref=${KGDIR}/ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
+#         excludemarkers=excludemarkers.txt \
+#         out=./beagle/${prefix}_imputed_chr${chrom}
+# done
+
+# # Remove duplicates
+# zcat data/imputed_23andme_merged.vcf.gz | grep -v "^#" | cut -f 3 | sort | uniq -c | \
+#    awk '{print $1 "\t" $2}' | awk '($1>1)' | grep -v "\." | cut -f 2 > dupids_23andme.txt
+
+# mkdir impute
+
