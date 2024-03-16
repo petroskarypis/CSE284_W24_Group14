12cab37930a075598dfb0706259be6b847bd16d2
Petros Karypis <petroskarypis@gmail.com>
2024-03-15 21:07:07 -0700

    working impute2

--- impute2.sh
+++ impute2.sh
@@ -1,7 +1,7 @@
 cd impute_v2.3.2_x86_64_static
 # TODO: Prepare the gens file and mimic the following
 # skipped stranding cmd: -strand_g ./Example/example.chr22.study.strand \
-./impute2 \
+valgrind --tool=massif --stacks=yes ./impute2 \
  -m ../data/genetic_map_chr1_combined_b37.txt \
  -h ../data/1000GP_Phase3_chr1.hap \
  -l ../data/1000GP_Phase3_chr1.legend \
